e5b539fdd7dd0a324f996ac25fb39572a8ae76fc
filiphuhta <filip.huhta@arcticgroup.se>
2021-06-29 10:15:46 +0200

    updated thumbnails (projects) with links

--- src/Projects.js
+++ src/Projects.js
@@ -3,6 +3,11 @@ import Thumbnail from './Thumbnail.js'; // Import the Thumbnail component
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Bachelor from './images/background-bachelor.jpg'
+import Bitcoin from './images/bitcoin-app.jpeg'
+import ImageGallery from './images/image-gallery.jpeg'
+import FirstReactApp from './images/react-app.jpeg'
+import Website from './images/website.jpeg'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,29 +31,45 @@ function Projects(props) {
         <Grid item xs={12} >
   
           <Thumbnail
-  link="/photoshop"
-  image="https://bookdown.org/gmli64/do_a_data_science_project_in_10_days/images/tree.PNG"
-  title="Photoshop Redesign"
-  category="Desktop App"
+  link="https://www.diva-portal.org/smash/record.jsf?pid=diva2%3A1435711&dswid=3040"
+  image={Bachelor}
+  title="Bachelors thesis"
+  text="Reactive systems : A solution for Wangiri fraud."
 />
         </Grid>
-        <Grid item xs={12}>
-          <Thumbnail
-  link="/photoshop"
-  image="https://bookdown.org/gmli64/do_a_data_science_project_in_10_days/images/tree.PNG"
-  title="Photoshop Redesign"
-  category="Desktop App"
+        <Grid item xs={12} >
+  
+  <Thumbnail
+link="https://github.com/filiphuhta/bitcoin-live-chart"
+image={Bitcoin}
+title="Cryptocurrency real-time pricing application"
+text="This is an application for displaying real-time cryptocurrency prices. The application is made with React and CoinCap API 2.0."
 />
-        
-        </Grid>
-        <Grid item xs={12}>
-          <Thumbnail
-  link="/photoshop"
-  image="https://bookdown.org/gmli64/do_a_data_science_project_in_10_days/images/tree.PNG"
-  title="Photoshop Redesign"
-  category="Desktop App"
+</Grid> <Grid item xs={12} >
+  
+  <Thumbnail
+link="https://github.com/filiphuhta/Image-gallery-app/"
+image={ImageGallery}
+title="Image gallery application"
+text="This is an application for displaying images on a webpage. It uses pure Javascript as Frontend and Node.js as backend. The application uses flickrs API to get the images."
 />
-        </Grid>
+</Grid> <Grid item xs={12} >
+  
+  <Thumbnail
+link="https://github.com/filiphuhta/my-first-react-app"
+image={FirstReactApp}
+title="My first React application"
+text="I have used the introduction to React tutorial, to build an interactive tic-tac-toe game. Which I have styled with bootstrap."
+/>
+</Grid> <Grid item xs={12} >
+  
+  <Thumbnail
+link="https://github.com/filiphuhta/filiphuhta-website"
+image={Website}
+title="My Website (filiphuhta.se)"
+text="This site was made by React, Babel, Material UI, Yarn and SASS."
+/>
+</Grid>
       </Grid>
     </div>
   );
--- src/Thumbnail.js
+++ src/Thumbnail.js
@@ -12,43 +12,36 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 500,
+    width: 500,
   },
   media: {
     height: 250,
   },
+  button: {
+    width: '100%',
+  }
 });
 
 function Thumbnail(props) {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
-    <CardActionArea>
-    <Link to={props.link}>
       <CardMedia
         className={classes.media}
         image={props.image}
         title="Project-img"
       />
-      </Link>
       <CardContent>
         <Typography gutterBottom variant="h5" component="h2">
-          Lizard
+        {props.title}
         </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
-          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-          across all continents except Antarctica
+        {props.text}
         </Typography>
-              <div className="project-title">{props.title}</div>
-        <div className="project-category">{props.category}</div>
       </CardContent>
-    </CardActionArea>
     <CardActions>
-      <Button size="small" color="secondary">
+      <Button href={props.link} className={classes.button} color="secondary">
         Read More
-      </Button>
-      <Button size="small" color="secondary">
-        Learn More
       </Button>
     </CardActions>
   </Card>
