9e2f8634657c81c7ce664fe9c08bf6b2a97df4fb
filiphuhta <filip.huhta@arcticgroup.se>
2021-06-29 11:04:50 +0200

    added some content to articles.js

--- src/Articles.js
+++ src/Articles.js
@@ -1,11 +1,37 @@
 import React from "react"
- 
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flex: 1,
+    padding: theme.spacing(5),
+    paddingTop: '10%', 
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  jumbotron: {
+    maxWidth: 700,
+  },
+  chip: {
+    margin: '0.5rem',
+    fontSize: '1rem',
+  },
+}));
 function Articles(props) {
-  return (
-    <div>
-      <h1>New Component</h1>
-    </div>
-  )
-}
+    const classes = useStyles();
+  
+    return (
+      <Grid container spacing={5} className={classes.root}
+      direction="column"
+      alignItems="center"
+      justify="center">
+          <Grid item xs={12} className={classes.jumbotron}>
+    <h1>Woops!</h1>
+    <p>This page is not done yet and under construction.</p>
+          </Grid>
+          </Grid>
+    )
+  }
  
 export default Articles;
